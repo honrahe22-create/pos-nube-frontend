40a2195990ebbfedca37ae67cfb67853550e5df9
Hugo Egas <honra.he22@gmail.com>
2026-08-22 07:52:24 -0500

    Evitar frontend antiguo y actualizar build POS NUBE

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+  },
+
+  server: {
+    headers: {
+      "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
+      Pragma: "no-cache",
+      Expires: "0",
+    },
+  },
+
+  preview: {
+    headers: {
+      "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
+      Pragma: "no-cache",
+      Expires: "0",
+    },
+  },
+});
